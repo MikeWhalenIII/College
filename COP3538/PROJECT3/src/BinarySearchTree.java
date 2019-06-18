edcdb5c935a110fd9b82c08e6b98ad4451c5a915
Mike Whalen <michael1327@hotmail.com>
2019-06-17 22:13:48 -0400

    Updated project files

--- COP3538/PROJECT3/src/BinarySearchTree.java
+++ COP3538/PROJECT3/src/BinarySearchTree.java
@@ -1,4 +1,190 @@
 
 public class BinarySearchTree {
 
+	public Node root; // Reference to the root
+	public static int nodesVisited; // Used to count the number of nodes visited
+									// during the find method.
+
+	public BinarySearchTree() {
+		root = null;
+	}
+
+	public void insert(String state, int population) {
+		Node newNode = new Node(state, population);
+		newNode.stateName = state;
+		newNode.statePopulation = population;
+
+		if (root == null) {
+			root = newNode;
+		} else {
+			Node current = root;
+			Node parent;
+			while (true) {
+				parent = current;
+				if (state.compareTo(current.stateName) < 0) {
+					current = current.leftChild; // Go left
+					if (current == null) {
+						parent.leftChild = newNode;
+						return;
+					}
+				} else {
+					current = current.rightChild; // Go right
+					if (current == null) {
+						parent.rightChild = newNode;
+						return;
+					}
+				}
+			}
+		}
+
+	}
+
+	public int find(String state) {
+		nodesVisited = 1;
+		Node current = root;
+
+		while (!current.stateName.equals(state)) {
+			if (state.compareTo(current.stateName) < 0) {
+				current = current.leftChild; // Go left
+				nodesVisited++;
+			} else {
+				current = current.rightChild; // Go right
+				nodesVisited++;
+			}
+			if (current == null) {
+				return -1;
+			}
+		}
+		return current.statePopulation;
+	}
+
+	public void delete(String state) {
+		Node current = root;
+		Node parent = root;
+		boolean isLeftChild = true;
+
+		while (!current.stateName.equals(state)) {
+			parent = current;
+			if (state.compareTo(current.stateName) < 0) {
+				isLeftChild = true;
+				current = current.leftChild;
+			} else {
+				isLeftChild = false;
+				current = current.rightChild;
+			}
+			if (current == null)
+				return;
+		}
+		// If no children, delete the node
+		if (current.leftChild == null && current.rightChild == null) {
+			if (current == root)
+				root = null;
+			else if (isLeftChild)
+				parent.leftChild = null;
+			else
+				parent.rightChild = null;
+		}
+		// If no right child, replace with the left subtree
+		else if (current.rightChild == null)
+			if (current == root)
+				root = current.leftChild;
+			else if (isLeftChild)
+				parent.leftChild = current.leftChild;
+			else
+				parent.rightChild = current.leftChild;
+
+		// If no left child, replace with the right subtree
+		else if (current.leftChild == null)
+			if (current == root)
+				root = current.rightChild;
+			else if (isLeftChild)
+				parent.leftChild = current.rightChild;
+			else
+				parent.rightChild = current.rightChild;
+		
+		// The node to be deleted has two children
+		else {
+			Node successor = getSuccessor(current);
+			if (current == root)
+				root = successor;
+			else if (isLeftChild)
+				parent.leftChild = successor;
+			else
+				parent.rightChild = successor;
+			successor.leftChild = current.leftChild;
+		}
+
+	}
+
+	private Node getSuccessor(Node delNode) {
+		Node successorParent = delNode;
+		Node successor = delNode;
+		Node current = delNode.rightChild;
+
+		while (current != null) {
+			successorParent = successor;
+			successor = current;
+			current = current.leftChild;
+		}
+		if (successor != delNode.rightChild) {
+			successorParent.leftChild = successor.rightChild;
+			successor.rightChild = delNode.rightChild;
+		}
+		return successor;
+	}
+
+	public void printInorder(Node localRoot) {
+		if (localRoot != null) {
+			printInorder(localRoot.leftChild);
+			localRoot.printNode();
+			printInorder(localRoot.rightChild);
+		}
+	}
+
+	public void printPreorder(Node localRoot) {
+		if (localRoot != null) {
+			localRoot.printNode();
+			printPreorder(localRoot.leftChild);
+			printPreorder(localRoot.rightChild);
+		}
+	}
+
+	public void printPostorder(Node localRoot) {
+		if (localRoot != null) {
+			printPostorder(localRoot.leftChild);
+			printPostorder(localRoot.rightChild);
+			localRoot.printNode();
+		}
+	}
+
+	public void printFiveMin() {
+
+	}
+
+	public void printFiveMax() {
+
+	}
+
+	/**
+	 * This class definition is used for the nodes in the binary search tree.
+	 * 
+	 * @author Michael Whalen (n01425161)
+	 * @version 1.0, 06/17/2019
+	 */
+	private class Node {
+		String stateName;
+		int statePopulation;
+		Node leftChild;
+		Node rightChild;
+
+		public Node(String state, int population) {
+			stateName = state;
+			statePopulation = population;
+		}
+
+		public void printNode() {
+			System.out.printf("%-25s%,10d\n", stateName, statePopulation);
+		}
+	}
+
 }
--- COP3538/PROJECT3/src/Project3.java
+++ COP3538/PROJECT3/src/Project3.java
@@ -1,9 +1,125 @@
+import java.io.*;
 
 public class Project3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static BinarySearchTree tree = new BinarySearchTree();
+
+	public static void main(String[] args) throws IOException {
+		int numStates = readFile();
+		welcomeMessage(numStates);
+
+		// Print BST (Inorder Traversal)
+		System.out.println("Inorder Traversal:\n");
+		System.out.printf("%-25s %-15s", "State Name", "State Population");
+		System.out.println("\n------------------------------------------");
+		tree.printInorder(tree.root);
+		
+
+		// Delete California
+		tree.delete("California");
+		System.out.println("\nCalifornia has been deleted from tree");
+		// Delete Florida
+		tree.delete("Florida");
+		System.out.println("Florida has been deleted from tree");
+		// Delete New York
+		tree.delete("New York");
+		System.out.println("New York has been deleted from tree");
+		
+
+		// Print BST (Preorder Traversal)
+		System.out.println("\nPreorder Traversal:\n");
+		System.out.printf("%-25s %-15s", "State Name", "State Population");
+		System.out.println("\n------------------------------------------");
+		tree.printPreorder(tree.root);
+		
+
+		// Find American Samoa
+		if (tree.find("American Samoa") >= 0)
+			System.out.printf("%s %,d\n", "\nAmerican Samoa is found with a population of", tree.find("American Samoa"));
+		else
+			System.out.println("American Samoa is not found");
+		System.out.println(BinarySearchTree.nodesVisited + " nodes visited");
+		
+		// Find Rhode Island
+		if (tree.find("Rhode Island") >= 0)
+			System.out.printf("%s %,d\n", "\nRhode Island is found with a population of", tree.find("Rhode Island"));
+		else
+			System.out.println("Rhode Island is not found");
+		System.out.println(BinarySearchTree.nodesVisited + " nodes visited\n");
+		
+		// Find Florida
+		if (tree.find("Florida") >= 0)
+			System.out.printf("%s %,d\n", "\nFlorida is found with a population of", tree.find("Florida"));
+		else
+			System.out.println("Florida is not found");
+		System.out.println(BinarySearchTree.nodesVisited + " nodes visited");
+		
+
+		// Delete U.S. Virgin Islands
+		tree.delete("U.S. Virgin Islands");
+		System.out.println("\nU.S. Virgin Islands has been deleted from tree");
+		// Delete Wyoming
+		tree.delete("Wyoming");
+		System.out.println("Wyoming has been deleted from tree");
+		// Delete West Virginia
+		tree.delete("West Virginia");
+		System.out.println("West Virginia has been deleted from tree");
+		// Delete New Mexico
+		tree.delete("Mexico");
+		System.out.println("Mexico has been deleted from tree");
+		
+
+		// Print BST (Postorder Traversal)
+		System.out.println("\nPostorder Traversal:\n");
+		System.out.printf("%-25s %-15s", "State Name", "State Population");
+		System.out.println("\n------------------------------------------");
+		tree.printPostorder(tree.root);
 
 	}
 
+	/**
+	 * This method displays a welcome message to the user with the number of
+	 * records imported.
+	 * 
+	 * @param numRecords
+	 *            takes in the number of records read in from the csv file.
+	 */
+	private static void welcomeMessage(int numNodes) {
+		System.out.println("COP3538 Project 3 - Michael Whalen\n" + "Instructor: Xudong Liu\n\n"
+				+ "Binary Search Trees\n\n" + "There were " + numNodes + " records put on the binary search tree.\n");
+	}
+
+	/**
+	 * This method reads the data from the specified CSV file. Then pushes the
+	 * states onto the stack.
+	 * 
+	 * @return Returns the number of states imported from the csv file.
+	 * @throws IOException
+	 *             Throws error if the cannot be read or cannot be found.
+	 */
+	private static int readFile() throws IOException {
+		int i = 0;
+
+		try {
+			BufferedReader readCSV = new BufferedReader(new FileReader("States3.csv"));
+
+			String fileRead = readCSV.readLine();
+
+			while ((fileRead = readCSV.readLine()) != null) {
+				String[] field = fileRead.split(",");
+
+				String tempStateName = field[0];
+				int tempPopulation = Integer.parseInt(field[3]);
+				{
+					tree.insert(tempStateName, tempPopulation);
+					i++;
+				}
+			}
+			readCSV.close(); // Close the reader
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+		}
+		return i; // return the number of records imported.
+	}
 }
