f315573e08219b716454a53ece0b31355b9a326a
MikeWhalenIII <michael1327@hotmail.com>
2020-10-07 22:22:00 -0400

    Add PartyInvites proj

---
+++ Web-Systems-Development/Assignments/Program1/Program1.cs
@@ -0,0 +1,59 @@
+﻿/* This program will divide, multiply and subtract two numbers.
+ * 
+ * Michael Whalen
+ * N01425161
+ * COP3855 - Web Systems Development
+ */
+
+using System;
+using System.Transactions;
+
+namespace Program1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double number1, number2 = 0;
+
+                Console.WriteLine("Welcome! Please provide two numbers that you would like to divide, multiply and subtract.\n");
+                Console.Write("First Number: ");
+                number1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Second Number: ");
+                number2 = Convert.ToDouble(Console.ReadLine());
+
+                Divide(number1, number2);
+                Multiply(number1, number2);
+                Subtract(number1, number2);
+
+                Console.WriteLine("\nPress CTRL + C to exit.\n");
+            }
+        }
+
+        static void Divide(double number1, double number2) 
+        {
+            double quotient = number1 / number2;
+            double remainder = number1 % number2;
+            
+            Console.WriteLine(number1 + " / " + number2 + " = " + "{0:0.00}", quotient);
+            Console.WriteLine(number1 + " % " + number2 + " = " + "{0:0.00}", remainder);
+        }
+
+        static void Multiply(double number1, double number2)
+        {
+            double product = number1 * number2;
+
+            Console.WriteLine(number1 + " * " + number2 + " = " + "{0:0.00}", product);
+        }
+
+        static void Subtract(double number1, double number2)
+        {
+            double difference = number1 - number2;
+
+            Console.WriteLine(number1 + " - " + number2 + " = " + "{0:0.00}", difference);
+        }
+    }
+}
---
+++ Web-Systems-Development/Assignments/Program1/Program2.cs
@@ -0,0 +1,43 @@
+﻿/* This program will compute the area and circumference of a circle.
+ * 
+ * Michael Whalen
+ * N01425161
+ * COP3855 - Web Systems Development
+ */
+
+using System;
+
+namespace Program1
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double radius = 0;
+
+                Console.WriteLine("Welcome! Please provide the radius of the circle.\n");
+                Console.Write("Radius: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+
+                Area(radius);
+                Circumference(radius);
+
+                Console.WriteLine("\nPress CTRL + C to exit.\n");
+            }
+        }
+
+        static void Area(double radius)
+        {
+            double area = 3.14 * radius * radius;
+            Console.WriteLine("Area: " + "3.14" + " * " + radius + " * " + radius + " = " + "{0:0.00}", area);
+        }
+
+        static void Circumference(double radius)
+        {
+            double circumference = 2 * 3.14 * radius;
+            Console.WriteLine("Circumference: " + "2" + " * " + "3.14" + " * " + radius + " = " + "{0:0.00}", circumference);
+        }
+    }
+}
