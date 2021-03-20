6044600bade72f165265ea4f163e0d139af87f06
MikeWhalenIII <michael1327@hotmail.com>
2021-03-19 22:23:39 -0400

    Adding readFile method

--- Graduate Data Stuctures and Algorithms/PROJECT1/src/com/project1/Main.java
+++ Graduate Data Stuctures and Algorithms/PROJECT1/src/com/project1/Main.java
@@ -1,9 +1,33 @@
 package com.project1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello");
+        // Display welcome message
+        welcomeMessage();
+
+        readFile();
+    }
+
+    private static void welcomeMessage() {
+        System.out.println("Welcome! COP5416 - Project I");
+    }
+
+    private static void readFile() throws FileNotFoundException {
+        Scanner fileReader = null;
+
+        try {
+            File dailyScoresTxt = new File("dailyScores.txt");
+            fileReader = new Scanner(dailyScoresTxt);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found. Check path.");
+        } finally {
+            fileReader.close();
+        }
     }
 }
