5bf6c93f8509d1555198688e4d53259f3dd5c1af
MikeWhalenIII <michael1327@hotmail.com>
2021-03-20 22:56:26 -0400

    Finished computeMeanAndSD()

--- Graduate Data Stuctures and Algorithms/PROJECT1/src/Main.java
+++ Graduate Data Stuctures and Algorithms/PROJECT1/src/Main.java
@@ -16,10 +16,7 @@ public class Main {
         // Read the txt file
         readFile();
         computeMeanAndSD();
-
-        //System.out.println(Arrays.toString(datesArray));
-        //System.out.println(Arrays.toString(scoreArray));
-
+        computeDifferences();
     }
 
     private static void welcomeMessage() {
@@ -42,7 +39,7 @@ public class Main {
                         count++;
                     }
                 } else {
-                    for(int j = 0; j < scoreArray.length; j++) {
+                    for (int j = 0; j < scoreArray.length; j++) {
                         scoreArray[j] = Double.parseDouble(fileReader.nextLine());
                         count++;
                     }
@@ -61,7 +58,7 @@ public class Main {
         System.out.println("\nMeans and Standard Deviations of Scores\n");
         System.out.printf("%-7s %-9s %-9s %-9s %-9s %-9s\n", "Student", datesArray[0], datesArray[1], datesArray[2], datesArray[3], datesArray[4]);
 
-        // Display score data
+        // Display all student score data
         for (int i = 0; i < 24; i++) {
             System.out.printf("%-10d %-9.0f %-9.0f %-9.0f %-9.0f %-9.0f\n", (i + 1), scoreArray[i], scoreArray[i + 24], scoreArray[i + 48], scoreArray[i + 72], scoreArray[i + 96]);
         }
@@ -70,15 +67,15 @@ public class Main {
         double mean1 = 0, mean2 = 0, mean3 = 0, mean4 = 0, mean5 = 0;
 
         for (int i = 0; i < scoreArray.length; i++) {
-            if (i < 24) {
+            if (i <= 23) {
                 mean1 += scoreArray[i];
-            } else if (i > 24 && i < 48) {
+            } else if (i >= 24 && i <= 47) {
                 mean2 += scoreArray[i];
-            } else if (i > 48 && i < 72) {
+            } else if (i >= 48 && i <= 71) {
                 mean3 += scoreArray[i];
-            } else if (i > 72 && i < 96) {
+            } else if (i >= 72 && i <= 95) {
                 mean4 += scoreArray[i];
-            } else if (i > 96 && i < 120) {
+            } else if (i >= 96 && i <= 119) {
                 mean5 += scoreArray[i];
             }
         }
@@ -94,6 +91,32 @@ public class Main {
         // Compute and display the Standard Deviation
         double sd1 = 0, sd2 = 0, sd3 = 0, sd4 = 0, sd5 = 0;
 
-        
+        for (int i = 0; i < scoreArray.length; i++) {
+            if (i <= 23) {
+                sd1 += Math.pow((scoreArray[i] - mean1), 2);
+            } else if (i >= 24 && i <= 47) {
+                sd2 += Math.pow((scoreArray[i] - mean2), 2);
+            } else if (i >= 48 && i <= 71) {
+                sd3 += Math.pow((scoreArray[i] - mean3), 2);
+            } else if (i >= 72 && i <= 95) {
+                sd4 += Math.pow((scoreArray[i] - mean4), 2);
+            } else if (i >= 96 && i <= 119) {
+                sd5 += Math.pow((scoreArray[i] - mean5), 2);
+            }
+        }
+        sd1 = Math.sqrt(sd1 / 24);
+        sd2 = Math.sqrt(sd2 / 24);
+        sd3 = Math.sqrt(sd3 / 24);
+        sd4 = Math.sqrt(sd4 / 24);
+        sd5 = Math.sqrt(sd5 / 24);
+
+        System.out.printf("%-10s %-9.2f %-9.2f %-9.2f %-9.2f %-9.2f\n", "Std Dev", sd1, sd2, sd3, sd4, sd5);
+    }
+
+    private static void computeDifferences() {
+        System.out.println("\nSignificant Differences in Mean Scores\n");
+        System.out.printf("%20s %-9s %-9s %-9s\n", datesArray[1], datesArray[2], datesArray[3], datesArray[4]);
+
+
     }
 }
